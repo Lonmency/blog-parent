82b414d5e9eac5662f43bf40a4e531ee24b77bce
lonmency <38210899@qq.com>
2022-01-19 12:39:00 +0800

    修改项目包名，日志换成logback

--- blog-api/src/main/java/com/lon/blog/BlogApp.java
+++ blog-api/src/main/java/com/lon/blog/BlogApp.java
@@ -1,13 +1,14 @@
 package com.lon.blog;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class BlogApp {
-
     public static void main(String[] args) {
+        log.info("Hey we go");
         SpringApplication.run(BlogApp.class,args);
     }
-
 }
